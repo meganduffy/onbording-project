8eb7f1cb9be518e65fc36204b54e4dc5a0890871
megan.duffy <meganemilyduffy@gmail.com>
2020-02-21 17:47:21 +0000

    Update Gemfile to include 4.0.0.beta4 and update pages_controller_spec.rb to request homepage

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe PagesController, type: :controller do
   describe 'GET #home' do
     context 'when viewing homepage in the browser' do
       it 'returns a 200 OK response' do
-        (expect(response.status).to eq(200))
+        get :home
+          expect(response.status).to eq(200)
       end
     end
   end
