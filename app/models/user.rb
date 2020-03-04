d7be1f2858bbe26c4a983982caef02290588a1ad
megan.duffy <meganemilyduffy@gmail.com>
2020-03-04 13:08:00 +0000

    Add validator to User model to check the validity of emails on the registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
 class User < ApplicationRecord
 
   has_secure_password
 
-  validates :first_name, :last_name, :email, :password_digest, :presence => true
-  validates :email, :uniqueness => true
+  validates :first_name, :last_name, :password_digest, :presence => true
+  validates :email, :presence => true, :uniqueness => true, :email => true
 
 end
