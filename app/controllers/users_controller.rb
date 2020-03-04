66b75a907a7379be7f035e785c056dc318c4554c
megan.duffy <meganemilyduffy@gmail.com>
2020-03-04 11:49:18 +0000

    Update users_controller.rb to accept user params and store them within the User table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
 end
