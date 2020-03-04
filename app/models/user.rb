9fa7db38964587f01060b5f7e3a76cfee8c768cf
megan.duffy <meganemilyduffy@gmail.com>
2020-03-04 13:59:09 +0000

    Update password validator to have minimum number of chars

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :first_name, :last_name, :password_digest, :presence => true
+  validates :first_name, :last_name, :presence => true
   validates :email, :presence => true, :uniqueness => true, :email => true
+  validates :password_digest, :length => { :minimum => 5 }, :presence => true
 
 end
