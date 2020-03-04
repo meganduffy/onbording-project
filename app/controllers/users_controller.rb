328a6c78138afa419dd5b24cc8480d6aa7e4bd7b
megan.duffy <meganemilyduffy@gmail.com>
2020-03-04 15:14:46 +0000

    Update user controller to allow form to display vaidation error messages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to '/'
     else
-      redirect_to '/signup'
+      render 'new'
     end
   end
 
