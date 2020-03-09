3107bc2348af6aa9c8a6496b4d624322b31a7d78
megan.duffy <meganemilyduffy@gmail.com>
2020-03-09 16:38:50 +0000

    Add unit testing to the User model

--- Gemfile
+++ Gemfile
@@ -28,6 +28,9 @@ gem 'bcrypt', '~> 3.1.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+# Form helper to assist with displaying error messages on forms
+gem "dynamic_form"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
@@ -41,6 +44,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'shoulda-matchers'
 end
 
 group :test do
@@ -49,7 +53,9 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
+  gem 'shoulda-matchers'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  user = User.new(first_name: "John",
+                  last_name: "Doe",
+                  email: "john@unique.com",
+                  password: "hard2guess")
+
+  it "is valid with all valid attributes" do
+    expect(user).to be_valid
+  end
+
+  it "is not valid without a first name" do
+    user.first_name = nil
+    expect(user).to_not be_valid
+  end
+
+  it "is not valid without a last name" do
+    user.last_name = nil
+    expect(user).to_not be_valid
+  end
+
+  it "is not valid without an email" do
+    user.email = nil
+    expect(user).to_not be_valid
+  end
+
+  it { validate_uniqueness_of(:email) }
+
+  it "is not valid without a properly formatted email" do
+    user.email = "notemail.com"
+    expect(user).to_not be_valid
+  end
+
+  it "is not valid without a password"do
+    user.password = nil
+    expect(user).to_not be_valid
+  end
+
+  it "is not valid without a password that is less than 5 characters" do
+    user.password = "1234"
+    expect(user).to_not be_valid
+  end
+
+
+
 end
