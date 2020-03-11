0e283bec5c93c502ead4cf27a56ed5d382ce5654
megan.duffy <meganemilyduffy@gmail.com>
2020-03-11 13:53:30 +0000

    Apply PR feedback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_143738) do
+ActiveRecord::Schema.define(version: 2020_03_11_125719) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "last_name"
