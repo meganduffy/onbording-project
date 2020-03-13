d7c0d9c18edb68499562c4572e90909dee9545ef
megan.duffy <meganemilyduffy@gmail.com>
2020-03-13 17:22:13 +0000

    Add logout functionality to sessions_controller.rb as well as update application.html.erb to display a logout button when logged in

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   get 'secret-page', to: 'pages#secret_page'
 end
