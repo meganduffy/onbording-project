2c9ecc6c3c15458a215499df124c8e37dfc1b054
megan.duffy <meganemilyduffy@gmail.com>
2020-03-16 12:32:37 +0000

    Remove all logout functionality in order to remove work intended for ATH-1116

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,9 +15,4 @@ class SessionsController < ApplicationController
     end
   end
 
-  def destroy
-    session[:user_id] = nil
-    redirect_to '/'
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
 
   get 'secret-page', to: 'pages#secret_page'
 end
