c1ea000d7b5f7c052ffc7c6ebb3fc919c71f6aef
megan.duffy <meganemilyduffy@gmail.com>
2020-03-19 11:11:54 +0000

    Created relationship between articles and users to facilitate article authors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_125719) do
+ActiveRecord::Schema.define(version: 2020_03_19_101737) do
+
+  create_table "articles", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
