ab5d4c22153607179e387a4e7724f40e65cd0095
megan.duffy <meganemilyduffy@gmail.com>
2020-03-19 10:44:14 +0000

    Fill in article_spec.rb model tests

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -4,7 +4,22 @@ RSpec.describe ArticlesController, type: :controller do
 
   describe 'POST #create' do
     context 'with correct params' do
-      it 'saves a new article to the database'
+
+      let(:existing_user) { create(:user) }
+
+      before(:each) do
+        allow(User).to receive(:find_by_email).
+            and_return(existing_user)
+        allow_any_instance_of(User).to receive(:authenticate).
+            and_return(existing_user)
+      end
+
+      it 'saves a new article to the database' do
+        expect {
+          post :create, params: existing_user
+        }.to change(Article, :count).by(1)
+      end
+
       it 'captures the user which created the article'
       it 'captures the time and date the article is created'
       it 'displays a success message'
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,9 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it 'is valid with all valid attributes'
-  it 'is not valid without a title'
-  it 'is not valid without content'
-  it 'is not valid without an author'
-  it 'is not valid without a timestamp'
+
+  article = Article.new(title: "New Article",
+                        content: "Contrary to popular belief, Lorem Ipsum is not simply random text.
+  It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+                        author: "megan.duffy@wpengine.com",
+                        created_at: "2020-03-10 12:26:10")
+
+  it 'is valid with all valid attributes' do
+    expect(article).to be_valid
+  end
+
+  it 'is not valid without a title' do
+    article.title = nil
+    expect(article).to_not be_valid
+  end
+
+  it 'is not valid without content' do
+    article.content = nil
+    expect(article).to_not be_valid
+  end
+
+  it 'is not valid without an author' do
+    article.author = nil
+    expect(article).to_not be_valid
+  end
+
+  it 'is not valid without a timestamp' do
+    article.created_at = nil
+    expect(article).to_not be_valid
+  end
 end
