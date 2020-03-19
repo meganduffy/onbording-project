ed0c29d8859352653868a340263e4d1b575b20bb
megan.duffy <meganemilyduffy@gmail.com>
2020-03-19 14:40:13 +0000

    Update articles controller tests to mock logged in user

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -5,28 +5,30 @@ RSpec.describe ArticlesController, type: :controller do
   describe 'POST #create' do
     context 'with correct params' do
 
-      let(:existing_user) { create(:user) }
+      let(:user) { create(:user) }
 
       before(:each) do
-        allow(User).to receive(:find_by_email).
-            and_return(existing_user)
-        allow_any_instance_of(User).to receive(:authenticate).
-            and_return(existing_user)
+        allow(controller).to receive(:current_user).and_return(user)
       end
 
       it 'saves a new article to the database' do
         expect {
-          post :create, params: existing_user
+          post :create, params: user
         }.to change(Article, :count).by(1)
       end
 
-      it 'captures the user which created the article'
-      it 'captures the time and date the article is created'
       it 'displays a success message'
+      it 'displays the article post'
     end
 
     context 'with incorrect params' do
-      it 'does not save a new article to the database'
+      it 'does not save a new article to the database' do
+        expect {
+          post :create, params: user
+        }.to_not change(Article, :count).by(1)
+      end
+
+      it 're-renders #new'
       it 'displays an error message'
     end
   end
