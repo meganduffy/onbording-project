526b95429a88173751d340db06307af6b6400618
megan.duffy <meganemilyduffy@gmail.com>
2020-03-19 10:31:07 +0000

    Sketch out article_spec.rb model tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with all valid attributes'
+  it 'is not valid without a title'
+  it 'is not valid without content'
+  it 'is not valid without an author'
+  it 'is not valid without a timestamp'
 end
