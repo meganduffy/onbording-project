22fa4e29f658aec8844060f09e59d6a491bb637e
megan.duffy <meganemilyduffy@gmail.com>
2020-03-20 12:03:22 +0000

    Create article controller logic to display form and create new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,10 +3,20 @@ class ArticlesController < ApplicationController
   before_action :require_user
 
   def new
-
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to '/'
+    else
+      render 'new'
+    end
+  end
 
+  private
+  def article_params
+    params.require(:article).permit(:title, :content).merge(user_id: current_user.id)
   end
 end
