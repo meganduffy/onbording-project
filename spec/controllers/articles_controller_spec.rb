b0225a2686478ca26fb7358a0956a4e19d9e55e9
megan.duffy <meganemilyduffy@gmail.com>
2020-03-23 12:12:48 +0000

    Working - update articles_controller_spec.rb to pass parameters correctly

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -43,28 +43,51 @@ RSpec.describe ArticlesController, type: :controller do
       let(:article) { create(:article) }
 
       before(:each) do
-        allow(controller).to receive(:current_user).and_return(User.find(article.user_id))
+        allow(controller).to receive(:current_user).and_return(article.user)
       end
+
+      let(:params) { {article: {title: article.title,
+                                content: article.content}} }
 
       it 'saves a new article to the database' do
         expect {
-          post :create, params: article
+          post :create, params: params
         }.to change(Article, :count).by(1)
       end
 
-      it 'displays a success message'
-      it 'displays the article post'
+      it 'displays a success message' do
+        post :create, params: params
+        expect( response.request.flash[:success] ).to_not be_nil
+      end
+
+      it 'pending: displays the article post'
 
       it 'redirects to homepage' do
-        post :create, params: article
+        post :create, params: params
         expect(response).to redirect_to '/'
       end
     end
 
     context 'with incorrect params' do
-      it 'does not save a new article to the database'
-      it 're-renders #new'
-      it 'displays an error message'
+
+      let(:article) { create(:article) }
+
+      before(:each) do
+        allow(controller).to receive(:current_user).and_return(article.user)
+      end
+
+      let(:params) { {article: {title: article.title,
+                                content: nil}} }
+
+      it 'does not save a new article to the database' do
+        expect {
+          post :create, params: params
+        }.to_not change(Article, :count)
+      end
+      it 're-renders #new' do
+        post :create, params: params
+        expect(response).to render_template "new"
+      end
     end
   end
 
