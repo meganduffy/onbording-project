6e6e4e66305f6bc9cdb138d8991b5f93228c492e
megan.duffy <meganemilyduffy@gmail.com>
2020-04-08 11:56:23 +0100

    Implement hash style rubocop fixes in apps

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,4 +1,4 @@
 class Session < ApplicationRecord
-  validates :email, :presence => true, :email => true
-  validates :password_digest, :length => { :minimum => 5 }, :presence => true
+  validates :email, presence: true, email: true
+  validates :password_digest, length: { minimum: 5 }, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :first_name, :last_name, :presence => true
-  validates :email, :presence => true, :uniqueness => true, :email => true
-  validates :password_digest, :length => { :minimum => 5 }, :presence => true
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, email: true
+  validates :password_digest, length: { minimum: 5 }, presence: true
 
   has_many :articles
 end
