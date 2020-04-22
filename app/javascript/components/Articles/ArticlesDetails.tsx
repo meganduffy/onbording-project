fceb8035b8e294ee4a81ccff984a57fc2f280b16
megan.duffy <meganemilyduffy@gmail.com>
2020-04-22 12:42:54 +0100

    Convert class components into stateless functional components

--- app/javascript/components/Articles/ArticlesDetails.tsx
+++ app/javascript/components/Articles/ArticlesDetails.tsx
@@ -1,27 +1,25 @@
-import React from "react"
+import React from 'react'; 
 
-type ArticleDetailsProps = {
-    title?:string,
-    content?:string,
-    user?:string,
-    created_at?:string
+export interface ArticleDetailsProps {
+  title:string,
+  content:string,
+  user:string,
+  created_at:string
 }
+ 
+const ArticleDetails: React.SFC<ArticleDetailsProps> = (props) => {
+  return ( 
+    <div id="articles-details">
+      <h3 id="articles-title">{props.title}</h3>
 
-class ArticleDetails extends React.Component<ArticleDetailsProps> {
-  render () {
-    return (
-        <div id="articles-details">
-            <h3 id="articles-title">{this.props.title}</h3>
+      <p id="articles-content">{props.content}</p>
 
-            <p id="articles-content">{this.props.content}</p>
-
-            <div id="articles-details-info">
-              <p id="articles-user">{this.props.user}</p>
-              <p id="articles-created-at">{this.props.created_at}</p>
-            </div>
-        </div>
-    );
-  }
+      <div id="articles-details-info">
+        <p id="articles-user">{props.user}</p>
+        <p id="articles-created-at">{props.created_at}</p>
+      </div>
+    </div>
+  );
 }
-
+ 
 export default ArticleDetails;
--- app/javascript/components/Articles/ArticlesHeader.tsx
+++ app/javascript/components/Articles/ArticlesHeader.tsx
@@ -1,19 +1,17 @@
-import React from "react"
+import React from 'react';
 
-type ArticleHeaderProps = {
-    title:string,
-    subtitle:string
+export interface ArticleHeaderProps {
+  title:string,
+  subtitle:string
 }
-
-class ArticleHeader extends React.Component<ArticleHeaderProps> {
-  render () {
-    return (
-        <div id="articles-header">
-            <h1>{this.props.title}</h1>
-            <h2>{this.props.subtitle}</h2>
+ 
+const ArticleHeader: React.SFC<ArticleHeaderProps> = (props) => {
+  return ( 
+    <div id="articles-header">
+            <h1>{props.title}</h1>
+            <h2>{props.subtitle}</h2>
         </div>
-    );
-  }
+  );
 }
-
+ 
 export default ArticleHeader;
