a1d06133ec3265c4e4ec549999fd5ae7066112d7
megan.duffy <meganemilyduffy@gmail.com>
2020-05-01 15:26:46 +0100

    Merge FullArticle and ArticlesDetails into one component

--- app/javascript/components/Articles/ArticlesDetails.tsx
+++ app/javascript/components/Articles/ArticlesDetails.tsx
@@ -1,25 +1,35 @@
-import React from 'react'; 
+import React from 'react';
 
 export interface ArticlesDetailsProps {
-  title:string,
-  content:string,
-  user:string,
-  created_at:string
+    title:string,
+    content:string,
+    firstName:string,
+    lastName?:string,
+    created_at:string,
+    link:string,
+    linkText:string
 }
- 
+
 const ArticlesDetails: React.FC<ArticlesDetailsProps> = (props) => {
-  return ( 
-    <div className="articles-details">
-      <h3 className="articles-title">{props.title}</h3>
+    return (
+        
+        <div className="outer-article-container">
+            <div className="articles-details">
+                <h2 className="articles-title">{props.title}</h2>
 
-      <p>{props.content}</p>
+                <p>{props.content}</p>
 
-      <div className="articles-details-info">
-        <p>{props.user}</p>
-        <p>{props.created_at}</p>
-      </div>
-    </div>
-  );
+                <div className="articles-details-info">
+                    <p>{props.firstName} {props.lastName}</p>
+                    <p>{props.created_at}</p>
+                </div>
+            </div>
+
+            <div className="articles-aside">
+                <a href={props.link}>{props.linkText}</a>
+            </div>
+        </div>
+    )
 }
- 
+
 export default ArticlesDetails;
