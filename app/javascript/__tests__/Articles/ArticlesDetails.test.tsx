2718978955c495566e9a7cdecf54c41c35bce39b
megan.duffy <meganemilyduffy@gmail.com>
2020-05-21 11:01:02 +0100

    install react testing library

--- app/javascript/__tests__/Articles/ArticlesDetails.test.tsx
+++ app/javascript/__tests__/Articles/ArticlesDetails.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
+import "@testing-library/jest-dom/extend-expect";
 import renderer from 'react-test-renderer';
 import ArticleDetails from '../../components/Articles/ArticlesDetails';
 
@@ -28,3 +29,16 @@ it('renders correctly', ()=>{
     editLink='/articles/3/edit'/>).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+it('displays an edit option when logged in', ()=>{
+  const tree = renderer.create(<ArticleDetails 
+    title='New Post' 
+    content='Ipsum Lorem'
+    firstName='Megan' 
+    link='google.com' 
+    linkText='Click Here' 
+    created_at='27 March 2020' 
+    editLink='/articles/3/edit'
+    currentUser={2}/>).toJSON();
+    expect(tree).toMatch(/Edit Article/)
+})
