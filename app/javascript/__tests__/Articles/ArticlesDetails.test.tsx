b409624f79c6c53c8bb24ccfc8a087765f134613
megan.duffy <meganemilyduffy@gmail.com>
2020-05-22 13:01:24 +0100

    Add another snapshot and tidy up

--- app/javascript/__tests__/Articles/ArticlesDetails.test.tsx
+++ app/javascript/__tests__/Articles/ArticlesDetails.test.tsx
@@ -6,25 +6,43 @@ import ArticleDetails from '../../components/Articles/ArticlesDetails';
 const expectedTitle='mechanism momentum museum minimum mushroom' 
 const expectedContent='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 const expectedFirstName='Megan' 
+const expectedLastName='Duffy'
 const expectedLink='/articles' 
 const expectedLinkText='View All Articles' 
 const expectedCreatedAt='27 March 2020' 
 const expectedEditLink='/articles/3/edit'
+const expectedCurrentUser = 3  
+const expectedEditLinkText = 'Edit Article'
 
 describe('ArticleDetails', () => {
   describe('Snapshots', () => {
-    test('should render the Article Details', () => {
+    test('should render the Article Details when logged out', () => {
         const { container } = render(
           <ArticleDetails title={expectedTitle}
           content={expectedContent}
           firstName={expectedFirstName}
+          lastName={expectedLastName}
+          link={expectedLink}
+          linkText={expectedLinkText}
+          createdAt={expectedCreatedAt} />
+        );
+        expect(container).toMatchSnapshot();
+      });
+
+      test('should render the Article Details when logged in', () => {
+        const { container } = render(
+          <ArticleDetails title={expectedTitle}
+          content={expectedContent}
+          firstName={expectedFirstName}
+          lastName={expectedLastName}
           link={expectedLink}
           linkText={expectedLinkText}
           createdAt={expectedCreatedAt}
-          editLink={expectedEditLink} />
+          editLink={expectedEditLink}
+          currentUser={expectedCurrentUser} />
         );
         expect(container).toMatchSnapshot();
-    });
+      });
   });
 
   describe('Props', () => {
@@ -33,14 +51,15 @@ describe('ArticleDetails', () => {
           <ArticleDetails title={expectedTitle}
           content={expectedContent}
           firstName={expectedFirstName}
+          lastName={expectedLastName}
           link={expectedLink}
           linkText={expectedLinkText}
-          createdAt={expectedCreatedAt}
-          editLink={expectedEditLink} />
+          createdAt={expectedCreatedAt} />
         );
         expect(container).toHaveTextContent(expectedTitle);
         expect(container).toHaveTextContent(expectedContent);
         expect(container).toHaveTextContent(expectedFirstName);
+        expect(container).toHaveTextContent(expectedLastName)
         expect(container).toHaveTextContent(expectedLinkText);
         expect(container).toHaveTextContent(expectedCreatedAt);
       });
@@ -51,6 +70,7 @@ describe('ArticleDetails', () => {
           <ArticleDetails title={expectedTitle}
           content={expectedContent}
           firstName={expectedFirstName}
+          lastName={expectedLastName}
           link={expectedLink}
           linkText={expectedLinkText}
           createdAt={expectedCreatedAt} />
@@ -59,13 +79,11 @@ describe('ArticleDetails', () => {
       });
 
       test('should render the edit option text if a current user is passed', () => {
-        const expectedCurrentUser = 3  
-        const expectedEditLinkText = 'Edit Article'
-
         const { container, getByText } = render(
           <ArticleDetails title={expectedTitle}
           content={expectedContent}
           firstName={expectedFirstName}
+          lastName={expectedLastName}
           link={expectedLink}
           linkText={expectedLinkText}
           createdAt={expectedCreatedAt}
